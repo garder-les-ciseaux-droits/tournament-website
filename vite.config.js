ce432819c68062cbde83af481e45f0564da51a4a
Evan Vosh <wahotondo@gmail.com>
2024-04-19 09:24:51 +0200

    fourth

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tournament-website/',
+  base: './tournament-website/',
   plugins: [vue()],
 })
